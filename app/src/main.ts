3b597f65d4f456d7bf25531d938e960af15e6800
Kaihang <wangz9096z@gmail.com>
2026-09-03 08:27:36 +0800

    change to tauri ig

--- app/src/main.ts
+++
@@ -1,6 +0,0 @@
-const button = document.querySelector<HTMLButtonElement>("#start");
-const results = document.querySelector<HTMLPreElement>("#results");
-
-button?.addEventListener("click", () => {
-  results!.textContent = "Benchmark starting...";
-});
